fa9b0b1f7e8590428bcd0431ec71e5c95822c1ed
B33pin <bipin.bhandari1510@gmail.com>
2020-05-31 21:10:36 +0545

    final

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,4 +1,8 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smartagro/Components/Reusable.dart';
+import 'package:smartagro/Screens/results.dart';
 import '../constants.dart';
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,18 +10,176 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
+  int ph =7;
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextField(
-        keyboardType: TextInputType.text,
-        textAlign: TextAlign.center,
-        style: kTextfieldTextStyle,
-        onChanged: (value) {
-
-        },
-        decoration: kTextFieldDecoration.copyWith(
-            hintText: 'Enter your full Name'
+    return SafeArea(
+      child: Container( padding: EdgeInsets.fromLTRB(20, 30, 10, 0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Submit Information', style: TextStyle(
+              color: Colors.lightBlueAccent, fontFamily: 'Righteous', fontSize: 22
+            ),),
+            SizedBox(
+              height: 6,
+            ),
+            ColumnSuper(alignment: Alignment.topRight,
+              innerDistance: -110,
+              children: <Widget>[
+                Text('Fill the correct information for requesting prediction and click on continue', style: TextStyle(
+                  fontSize: 15, color: Colors.black54, fontFamily: 'Righteous'
+                ),),
+                Image(height: 100,width: 90,image: AssetImage('images/agro.png')),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Divider(thickness: 1,),
+            Row(
+              children: <Widget>[
+                Text('Enter Your Name', style: TextStyle(fontFamily: 'Righteous', fontSize: 21,color: Colors.black54),),
+                SizedBox(width: 10,),
+                Icon(Icons.streetview, color: Colors.black54,size: 20,),
+              ],
+            ),
+            SizedBox(height: 5,),
+            TextField(textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                    borderSide: BorderSide(width: 0.8,
+                        color: Colors.lightBlueAccent),),
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: 15.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                    borderSide: BorderSide(width: 0.8),),
+                  hintText: ' Enter Your Name',
+              ),
+            ),
+            SizedBox(height: 10,),
+            Row(
+              children: <Widget>[
+                Text('Enter Your ID', style: TextStyle(fontFamily: 'Righteous', fontSize: 21,color: Colors.black54),),
+                SizedBox(width: 10,),
+                Icon(Icons.account_circle, color: Colors.black54,size: 20,),
+              ],
+            ),
+            SizedBox(height: 5,),
+            TextField(keyboardType: TextInputType.number,textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                fillColor: Colors.grey[100],
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                  borderSide: BorderSide(width: 0.8,
+                      color: Colors.lightBlueAccent),),
+                contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                  borderSide: BorderSide(width: 0.8),),
+                hintText: ' Enter Your ID',
+              ),
+            ),
+            Divider(thickness: 1,),
+            Container(
+              child: Reusable_Card(
+                  colour: Colors.grey[200],
+                  CardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 5,),
+                      Text(
+                        "Select PH",style: TextStyle(fontSize: 20, fontFamily: 'Righteous', color: Colors.black54),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: <Widget>[
+                          Text(ph.toString(),style: TextStyle(color: Colors.black, fontFamily: 'Righteous', fontSize: 30),),
+                        ],
+                      ),
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          activeTrackColor: Colors.white,
+                          thumbColor: Color(0xFFEB1555),
+                          overlayColor: Color(0x29EB1555),
+                          thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                          overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                        ),
+                        child: Slider(
+                            value: ph.toDouble(),
+                            min: 0,
+                            max: 10,
+                            activeColor: Colors.blue,
+                            inactiveColor: Color(0xFF8D8E98),
+                            onChanged: (double newValue) {
+                              setState(() {
+                                ph = newValue.round();
+                              });
+                            }),
+                      ),
+                      GestureDetector(onTap: (){},child: Text("Don't have ph? Select 0, Request here!", style: TextStyle(fontFamily: 'Righteous'),)),
+                      SizedBox(height: 7,),
+                    ],
+                  )),
+            ),
+            Divider(thickness: 1,),
+            SizedBox(height: 10,),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text("${selectedDate.toLocal()}".split(' ')[0], style: TextStyle(fontSize: 22, fontFamily: 'Righteous'),),
+                  SizedBox(height: 10.0,),
+                  RaisedButton(
+                    onPressed: () => _selectDate(context),
+                    child: Text('Select date'),
+                  ),
+                ],
+              ),
+            ),
+            Divider(thickness: 1,),
+            SizedBox(height: 25,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.grey[100],
+                  onPressed: () => _selectDate(context),
+                  child: Text('Cancel'),
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: () {Navigator.push(context, new MaterialPageRoute(
+                      builder: (context) => Results()
+                  ));},
+                  child: Text('Continue'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/Screens/results.dart
+++ lib/Screens/results.dart
@@ -1,13 +1,176 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 class Results extends StatefulWidget {
   @override
   _ResultsState createState() => _ResultsState();
 }
 
 class _ResultsState extends State<Results> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+String i;
+bool loading =true;
+List userData;
+
+Future<String> _getData() async {
+var response = await http.get("https://fsocietyspaceapp.herokuapp.com/api/?username=nirjalpaudel&password=n1_rjal9841&name=Nirjal+Paudel&date-to-predict=2020-07-12&place=Kathmandu");
+var getData = json.decode(response.body);
+if (this.mounted)
+setState(() {
+loading = false;
+userData = [getData];
+});
+}
+
+// @override
+// void initState() {
+//   super.initState();
+//  // getData();
+// }
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text("Crop Growth Prediction"),
+backgroundColor: Colors.green,
+actions: <Widget>[
+IconButton(icon: Icon(Icons.refresh), onPressed: (){
+_getData();
+}
+),
+],
+),
+body: ListView(
+padding: EdgeInsets.all(10.0),
+children: <Widget>[
+loading ? Center(child: CircularProgressIndicator()):
+ListView.builder(
+shrinkWrap: true,
+physics: NeverScrollableScrollPhysics(),
+itemCount: userData == null ? 'Empty' : userData[0]["cropsinfo"].length,
+itemBuilder:(context,i){
+return cropdata(i);
+}
+)
+],
+
+)
+);
+}
+Widget cropdata(int i ){
+return Column(
+children:<Widget>[
+Image(image: AssetImage('img/wheat.jpeg')),
+Center(child: Text(userData[0]["cropsinfo"][i]["name"],style: TextStyle(
+fontSize:20.0,fontWeight:FontWeight.bold
+),)
+),
+SizedBox(height:10.0),
+
+Row(
+children: <Widget>[
+Expanded(
+child: Container(
+color:Colors.red,
+//color:Colors.blue,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+Text(userData[0]["cropsinfo"][i]["maxtemp"].toString(),style: TextStyle(fontWeight:FontWeight.bold,fontSize:15.0),),
+Image(image: AssetImage('img/wheat.jpeg')),
+Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 5),),
+Text('maxtemp',style: TextStyle(fontSize:13.0,fontWeight:FontWeight.bold),)
+],
+),
+),
+),
+Padding(padding:EdgeInsets.all(10.0)),
+Expanded(
+child: Container(
+color:Colors.green,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+Text(userData[0]["cropsinfo"][i]["mintemp"].toString(),style: TextStyle(fontWeight:FontWeight.bold,fontSize:15.0),),
+Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 5),),
+Text('mintemp',style: TextStyle(fontSize:13.0,fontWeight:FontWeight.bold),)
+],
+),
+),
+),
+Padding(padding:EdgeInsets.all(10.0)),
+Expanded(
+child: Container(
+color:Colors.blue,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+Text(userData[0]["cropsinfo"][i]["maxppt"].toString(),style: TextStyle(fontWeight:FontWeight.bold,fontSize:15.0),),
+Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 5),),
+Text('maxppt',style: TextStyle(fontSize:13.0,fontWeight:FontWeight.bold),)
+],
+),
+),
+),
+
+
+],
+),
+SizedBox(height:5.0),
+Row(
+children: <Widget>[
+Expanded(
+child: Container(
+color:Colors.orange,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+Text(userData[0]["cropsinfo"][i]["minppt"].toString(),style: TextStyle(fontWeight:FontWeight.bold,fontSize:15.0),),
+Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 5),),
+Text('minppt',style: TextStyle(fontSize:13.0,fontWeight:FontWeight.bold),)
+],
+),
+),
+),
+Padding(padding:EdgeInsets.all(10.0)),
+Expanded(
+child: Container(
+color:Colors.blue,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+Text('kathmandu',style: TextStyle(fontWeight:FontWeight.bold,fontSize:13.0),),
+Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 5),),
+Text('Place',style: TextStyle(fontSize:13.0,fontWeight:FontWeight.bold),)
+],
+),
+),
+),
+Padding(padding:EdgeInsets.all(10.0)),
+Expanded(
+
+child: Container(
+
+color:Colors.blue,
+padding:EdgeInsets.all(20.0),
+child: Column(
+children: <Widget>[
+//InkWell(onTap: (){},)
+//IconButton(icon: Icon(Icons.replay), onPressed: (){launch('https://en.wikipedia.org/wiki/Wheat');}),
+// RaisedButton(onPressed: (){launch('https://en.wikipedia.org/wiki/Wheat');},
+// ),
+],
+),
+),
+),
+
+
+],
+),
+],
+);
+
+}
 }
